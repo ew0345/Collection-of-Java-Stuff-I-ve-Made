c53aef468c735eeb806aa54bd11d362f4afebe5d
ew0345 <ew0345@users.noreply.github.com>
2020-07-13 16:45:35 -0400

    Update Rainbow.java

--- Rainbow.java
+++ Rainbow.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 * Author: Ew0345
 * Version: 1.4
 * Update Log:
-* 	1.4 - Added ability to skip the choice section w/ command-line args & if you choose to not start, the program will exit without asking for a delay.
+* 	1.4 - Added ability to skip the choice section w/ command-line args & if you choose to not start, 
+		the program will exit without asking for a delay.
 *	1.3 - Added ability for user to decide the delay time between color change.
 *	1.2 - Separated the screen clear into it's own method and called it before asking to start to clear any previous output.
 *	1.1 - Added rerun main method on invalid answer (not yes/no)
@@ -16,7 +17,7 @@ public class Rainbow {
         try (Scanner a = new Scanner(System.in)) {
             clearScreen();
 
-            String s = "n";
+            String s = "";
             int t = 0;
 
 			if (args.length >= 1) {
@@ -27,7 +28,25 @@ public class Rainbow {
             	    } else if (arg.toLowerCase().contains("ep")) {
             	        s = "y";
             	        t = 10;
-            	    }	
+            	    } else if (arg.toLowerCase().contains("info")) {
+						System.out.println("Author: Ew0345\n"
+							+"Version: 1.4\n"
+							+"Update Log:\n"
+							+"1.4 - Added ability to skip the choice section w/ command-line args & if you choose to not start, " 
+								+"the program will exit without asking for a delay.\n"
+							+"1.3 - Added ability for user to decide the delay time between color change.\n"
+							+"1.2 - Separated the screen clear into it's own method and called it before asking to start to clear any previous output.\n"
+							+"1.1 - Added rerun main method on invalid answer (not yes/no)"
+							+"1.0 - Released"
+						);
+						System.out.print("Start: ");
+						s = a.nextLine();
+
+						if (s.equalsIgnoreCase("y") || s.equalsIgnoreCase("yes")) {
+							System.out.print("Delay (ms): ");
+							t = a.nextInt();
+						}
+					}
 				}
 			} else {
 				System.out.print("Start: ");
@@ -37,6 +56,12 @@ public class Rainbow {
 					System.out.print("Delay (ms): ");
 					t = a.nextInt();
 				}
+			}
+			
+			if (s == null || s.isEmpty()) {
+				s = "invalid";
+				System.out.print("YES/NO only");
+				Thread.sleep(1000);
 			}
 
             switch(s.toLowerCase()) {
